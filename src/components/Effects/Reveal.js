5936e002415c5fe7aea807f3f6cf301bbe7fcd2d
jan <jan@mrozilla.cz>
2017-07-05 15:43:28 +0200

    update reveal effect component

--- src/components/Effects/Reveal.js
+++ src/components/Effects/Reveal.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
 
 // Styles
 import styled from 'styled-components';
-import { colour, transition, animation } from '../../utils/styles';
+import { colour, transition, animation, positionAbsolute } from '../../utils/styles';
 
 // Helpers
 import { withScrollPosition } from '../../utils/helpers';
@@ -20,63 +20,59 @@ import { withScrollPosition } from '../../utils/helpers';
 // Container
 const RevealContainer = styled.span`
   position: relative;
-  display: ${props => props.isBlock ? 'block' : 'inline-block'};
+  display: ${props => props.isBlock ? 'block' : 'inline-block' };
   animation-delay: ${props => props.delay};
-`; // TODO improve props.block logic
-
-RevealContainer.defaultProps = {
-  delay:     '0ms',
-  direction: 'right',
-  block:     false,
-};
+  &:after {
+    content: '';
+    pointer-events: none;
+    ${ positionAbsolute() }
+    background-color: ${colour.brand.primary};
+    ${props => props.isInViewport 
+      ? `animation: ${animation[props.animation][props.direction]} ${transition.primary.long} both;`
+      : 'opacity: 0;'
+    }
+    animation-delay: inherit;
+  }
+`;
 
 // Content
 const RevealContent = styled.span`
-  ${props => props.isBlock && 'display: block'};
+  display: ${props => props.isBlock ? 'block' : 'inline-block' };
   ${props => props.isInViewport
-  ? `animation: ${animation.appear} ${transition.primary.long} both;`
-  : 'opacity: 0;'
+    ? `animation: ${animation.appear} ${transition.primary.long} both;`
+    : 'opacity: 0;'
   }
   animation-delay: inherit;
-`; // TODO improve props.block logic
-
-// Box
-const RevealBox = styled.span`
-  position: absolute;
-  top: 0;bottom: 0;left: 0;right: 0;
-  width: 120%; 
-  pointer-events: none;
-  height: ${props => props.isBlock ? '100%' : '120%'};
-  background-color: ${colour.brand.primary};
-  will-change: width;
-  ${props => props.isInViewport
-  ? `animation: ${animation.reveal.right} ${transition.primary.long} both;`
-  : 'width: 0%;'
-  }
-  animation-delay: inherit;
-`; // TODO improve props.block logic
+`;
 
 // =============================================================================
 // Component
 // =============================================================================
 
-function RevealWrapper({ children, delay, isInViewport, isBlock }) {
+function RevealWrapper({ children, ...rest }) {
   return (
-    <RevealContainer isBlock={isBlock} isInViewport={isInViewport} delay={delay}>
-      <RevealContent isBlock={isBlock} isInViewport={isInViewport}>{children}</RevealContent>
-      <RevealBox isBlock={isBlock} isInViewport={isInViewport} />
+    <RevealContainer {...rest}>
+      <RevealContent {...rest}>{children}</RevealContent>
     </RevealContainer>
   );
 }
 
-RevealWrapper.propTypes = {
-  // children: PropTypes.element.isRequired,
+const Reveal = withScrollPosition(RevealWrapper);
+
+Reveal.propTypes = {
+  children: PropTypes.node.isRequired,
   delay: PropTypes.string,
+  animation: PropTypes.string,
+  direction: PropTypes.string,
   isInViewPort: PropTypes.bool,
   isBlock: PropTypes.bool,
 };
 
-const Reveal = withScrollPosition(RevealWrapper);
+Reveal.defaultProps = {
+  delay:     '0ms',
+  animation: 'slide',
+  direction: 'right',
+};
 
 // =============================================================================
 // Export
