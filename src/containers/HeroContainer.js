c36c6fc7012f4032f3e96e29332a4a923813a29d
jan <jan@mrozilla.cz>
2017-07-07 22:39:40 +0200

    add hero container

---
+++ src/containers/HeroContainer.js
@@ -0,0 +1,57 @@
+// =============================================================================
+// Import
+// =============================================================================
+
+// React
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Components
+import { Container } from '../components/Layout';
+import { Heading, Subheading, Text } from '../components/Typography';
+import { Reveal, Wiggle } from '../components/Effects';
+
+// =============================================================================
+// Component
+// =============================================================================
+
+export default function HeroContainer({ content: { title, subtitle, text }, filter }) {
+  return (
+    <Container>
+      <Wiggle>
+        <Heading>
+          <Reveal delay="250ms" key={filter}>
+            {filter && filter} {title}
+          </Reveal>
+        </Heading>
+      </Wiggle>
+      <Wiggle>
+        <Subheading marginBottom="3rem">
+          <Reveal delay="500ms">
+            {subtitle}
+          </Reveal>
+        </Subheading>
+      </Wiggle>
+      <Wiggle>
+        <Text marginBottom="3rem">
+          <Reveal delay="750ms">
+            {text}
+          </Reveal>
+        </Text>
+      </Wiggle>
+    </Container>
+  );
+}
+
+HeroContainer.propTypes = {
+  content: PropTypes.shape({
+    title:    PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    text:     PropTypes.string.isRequired,
+  }).isRequired,
+  filter: PropTypes.string,
+};
+
+HeroContainer.defaultProps = {
+  filter: undefined,
+};
