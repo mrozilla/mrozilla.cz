7ad373e5cfc34189a6a8d957871fcc803cde436d
jan <jan@mrozilla.cz>
2017-07-18 00:06:42 +0200

    add Input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,8 +3,8 @@
 // =============================================================================
 
 // React
-// import React from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Styles
 import glamorous from 'glamorous';
@@ -24,6 +24,7 @@ export const TextArea = glamorous.textarea(
     color:            'inherit',
     lineHeight:       'inherit',
     borderRadius:     border.radius.small,
+    outline:          '0',
     '&::placeholder': {
       opacity: 0.5,
     },
@@ -33,6 +34,43 @@ export const TextArea = glamorous.textarea(
   }),
 );
 
+const TextInput = glamorous.input({
+  WebkitAppearance: 'none',
+  outline:          'none',
+  border:           'none',
+  fontFamily:       'inherit',
+  color:            'inherit',
+});
+
 // =============================================================================
 // Component
 // =============================================================================
+
+export function Input({ type, name, value, onChange }) {
+  const types = {
+    text: (
+      <TextInput
+        type="text"
+        name={name}
+        value={value}
+        onChange={e => onChange(name, e.target.value)}
+      />
+    ),
+    number: (
+      <TextInput
+        type="number"
+        name={name}
+        value={value}
+        onChange={e => onChange(name, parseInt(e.target.value, 10))}
+      />
+    ),
+  };
+  return types[type];
+}
+
+Input.propTypes = {
+  type:     PropTypes.string.isRequired,
+  value:    PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.number.isRequired]).isRequired,
+  name:     PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
