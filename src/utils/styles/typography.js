f16222f13d09719f3d422ecff0f87967dbf5a84a
jan <jan@mrozilla.cz>
2017-07-18 21:24:15 +0200

    add fonts

--- src/utils/styles/typography.js
+++ src/utils/styles/typography.js
@@ -2,7 +2,45 @@
 // Import
 // =============================================================================
 
+import { css } from 'glamor';
 import { color } from '../styles';
+
+import nunitoLight from '../../utils/fonts/nnt-300.woff2';
+import nunitoBlack from '../../utils/fonts/nnt-900.woff2';
+import robotoMono from '../../utils/fonts/rbtmn-400.woff2';
+import robotoMonoBold from '../../utils/fonts/rbtmn-700.woff2';
+
+// =============================================================================
+// Fonts
+// =============================================================================
+
+css.fontFace({
+  fontFamily: 'Nunito',
+  fontStyle:  'normal',
+  fontWeight: 300,
+  src:        `local('Nunito Light'), url('${nunitoLight}') format('woff2')`,
+});
+
+css.fontFace({
+  fontFamily: 'Nunito',
+  fontStyle:  'normal',
+  fontWeight: 900,
+  src:        `local('Nunito Black'), url('${nunitoBlack}') format('woff2')`,
+});
+
+css.fontFace({
+  fontFamily: 'Roboto Mono',
+  fontStyle:  'normal',
+  fontWeight: 400,
+  src:        `local('Roboto Mono'), url('${robotoMono}')`,
+});
+
+css.fontFace({
+  fontFamily: 'Roboto Mono',
+  fontStyle:  'normal',
+  fontWeight: 700,
+  src:        `local('Roboto Mono Bold'), url('${robotoMonoBold}')`,
+});
 
 // =============================================================================
 // Typography
