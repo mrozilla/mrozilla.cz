137e52e892842e43818c39933e46e9cc4b4872ac
jan <jan@mrozilla.cz>
2017-07-28 23:12:57 +0200

    refactor content

---
+++ src/utils/content/about.js
@@ -0,0 +1,87 @@
+// =============================================================================
+// Import
+// =============================================================================
+
+// React
+import React from 'react';
+
+// Components
+import { Link } from '../../components/Navigation/Link';
+
+// =============================================================================
+// Content
+// =============================================================================
+
+const about = {
+  top: {
+    title:    'about',
+    subtitle: 'the details, historical data, and some other information.',
+    text:     (
+      <span>
+        My name is Jan. I&apos;m originally from <s>Czechia</s> the Czech
+        Republic, lived in Finland and the United Kingdom, and currently
+        residing in Vienna, Austria. I&apos;ve got <Link to="">a CV</Link> or
+        you can read more <Link to="#below">below ⇣</Link>.
+      </span>
+    ),
+  },
+  text: [
+    <span>
+      I build digital products. Everything from concepts to UX, UI, design and
+      development. I&apos;ve done some <Link to="/work">work →</Link> in the
+      past, and will probably do some <Link to="/lab">more →</Link>.
+    </span>,
+    <span>
+      I&apos;ve managed <Link to="https://esn.org">big creative teams</Link> (up
+      to 100 people),{' '}
+      <Link to="https://movineurope.esn.org">small product teams</Link>, and
+      also have been at the very bottom of an organisation as a junior
+      developer. I&apos;m a{' '}
+      <Link to="https://himoment.com">startup co-founder</Link>. I do a lot of
+      volunteering and some open-source work. I&apos;m a{' '}
+      <Link to="https://medium.com/TODO">
+        keynote speaker and a workshop faciliator
+      </Link>.
+    </span>,
+    <span>
+      I design in Adobe Photoshop, Adobe Illustrator, Adobe XD, Adobe InDesign,
+      Adobe Lightroom, Adobe Premiere, and Adobe After Effects.
+    </span>,
+    <span>
+      I code in Javascript:{' '}
+      <Link to="https://github.com/facebook/react">React</Link>,{' '}
+      <Link to="https://github.com/reactjs/redux">Redux</Link>,{' '}
+      <Link to="https://github.com/paypal/glamorous">Glamorous</Link> (css-in-js
+      ftw), <Link to="https://github.com/nodejs/node">Node</Link> &{' '}
+      <Link to="https://github.com/nodejs/node">Express</Link>. I usually edit
+      that code in <Link to="https://github.com/facebook/nuclide">
+        Nuclide
+      </Link>, run it through{' '}
+      <Link to="https://github.com/prettier/prettier">Prettier</Link> and{' '}
+      <Link to="http://www.syntevo.com/smartgit/">SmartGit</Link>, and post it
+      on <Link to="https://github.com/mrozilla/">Github</Link>.
+    </span>,
+    <span>
+      I happen to have{' '}
+      <Link to="https://www.ntu.ac.uk/study-and-courses/courses/find-your-course/business/pg/2017-18/european-business-and-finance">
+        M.Sc.
+      </Link>{' '}
+      and{' '}
+      <Link to="https://www.fbm.vutbr.cz/en/applicants/european-business-and-finance">
+        Ing.
+      </Link>{' '}
+      degrees in European Business and Finance for some reason.
+    </span>,
+    <span>
+      You can reach to me on{' '}
+      <Link to="https://twitter.com/mrozilla">Twitter</Link>,{' '}
+      <Link to="https://medium.com/mrozilla">Medium</Link>,{' '}
+      <Link to="https://github.com/mrozilla">Github</Link>,{' '}
+      <Link to="https://linkedin.com/in/mrozilla">Linkedin</Link>, or just{' '}
+      <Link to="mailto:jan@mrozilla.cz">send me an email</Link>. That's probably
+      the best chance to get a response.
+    </span>,
+  ],
+};
+
+export default about;
---
+++ src/utils/content/home.js
@@ -0,0 +1,49 @@
+// =============================================================================
+// Import
+// =============================================================================
+
+// React
+import React from 'react';
+
+// Components
+import { Link } from '../../components/Navigation/Link';
+
+// =============================================================================
+// Content
+// =============================================================================
+
+const home = {
+  top: {
+    title:    'mrozilla.',
+    subtitle: `the online home of
+    a one-man digital studio.`,
+    text: (
+      <span>
+        Always busy building new digital experiences, <br />
+        mixing together design, development, and copywriting. <br />
+        Have a look at some <Link to="#below">case studies ⇣</Link> or all the{' '}
+        <Link to="/work">work →</Link>
+      </span>
+    ),
+  },
+  bottom: {
+    title:    'cool.',
+    subtitle: `these were the highlights.
+    care for some more?`,
+    text: (
+      <span>
+        There’s a{' '}
+        <Link to="https://medium.com/mrozilla/" target="_blank">
+          blog to read
+        </Link>{' '}
+        if you like reading. <br />
+        I also do a lot of <Link to="/lab">experiments →</Link> if you’re into
+        that stuff. <br />
+        Or send me a message if you’d like to build something together. <br />
+        It’s <Link to="mailto:jan@mrozilla.cz">jan@mrozilla.cz</Link>
+      </span>
+    ),
+  },
+};
+
+export default home;
