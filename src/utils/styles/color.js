0cdc81ca826976bcc2a3e907c8df151f51ebcbf9
jan <jan@mrozilla.cz>
2017-08-26 23:24:39 +0200

    rewrite colour system in HSL

--- src/utils/styles/color.js
+++ src/utils/styles/color.js
@@ -1,52 +1,32 @@
 // =============================================================================
-// Import
-// =============================================================================
-
-import { complement, desaturate, lighten, darken } from 'polished';
-
-// =============================================================================
 // Styles
 // =============================================================================
 
+const hue = 354;
+const sat = 82;
+const lig = 55;
+
 const colour = {
   brand: {
-    primary: '#ea2e42',
-    get secondary() {
-      return darken(0.1, this.primary);
-    },
-    success: '#5cb85c',
-    info:    '#5bc0de',
-    warning: '#f0ad4e',
-    danger:  '#d9534f',
-    get complement() {
-      return complement(this.primary);
-    },
+    primary:    `hsl(${hue}, ${sat}%, ${lig}%)`,
+    secondary:  `hsl(${hue}, ${sat}%, ${lig - 10}%)`,
+    success:    '#5cb85c',
+    info:       '#5bc0de',
+    warning:    '#f0ad4e',
+    danger:     '#d9534f',
+    complement: `hsl(${Math.abs(hue - 180)}, ${sat}%, ${lig}%)`,
     get gradient() {
       return `linear-gradient(45deg, ${this.primary}, ${this.secondary})`;
     },
   },
   grey: {
-    get basic() {
-      return desaturate(0.75, colour.brand.primary); // ~'#55595c'
-    },
-    get lightest() {
-      return lighten(0.43, this.basic); // ~#f7f7f9
-    },
-    get lighter() {
-      return lighten(0.4, this.basic); // ~#f7f7f9
-    },
-    get light() {
-      return lighten(0.3, this.basic); // ~#818a91
-    },
-    get dark() {
-      return darken(0.15, desaturate(0.05, this.basic)); // ~#373a3c
-    },
-    get darker() {
-      return darken(0.42, this.basic); // FIXME add target colour
-    },
-    get darkest() {
-      return darken(0.47, this.basic); // FIXME add target colour
-    },
+    basic:    `hsl(${hue}, ${sat - 75}%, ${lig}%)`,
+    lightest: `hsl(${hue}, ${sat - 75}%, ${lig + 43}%)`,
+    lighter:  `hsl(${hue}, ${sat - 75}%, ${lig + 40}%)`,
+    light:    `hsl(${hue}, ${sat - 75}%, ${lig + 30}%)`,
+    dark:     `hsl(${hue}, ${sat - 80}%, ${lig - 15}%)`,
+    darker:   `hsl(${hue}, ${sat - 75}%, ${lig - 42}%)`,
+    darkest:  `hsl(${hue}, ${sat - 75}%, ${lig - 47}%)`,
   },
   get background() {
     return this.grey.lightest;
@@ -54,7 +34,7 @@ const colour = {
 };
 
 // =============================================================================
-// Styles
+// Export
 // =============================================================================
 
 export default colour;
