b8c325427123005fecac4326099ae0f001e3433d
jan <jan@mrozilla.cz>
2018-04-16 23:41:22 +0200

    add navbar content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,21 +5,41 @@
 import React from 'react';
 import './index.css';
 
-import { Wrapper, Header, Heading, Link } from '../components';
+import { Wrapper, Header, Heading, Link, Nav } from '../components';
 
 // ─────────────────────────────────────────────────────────────────────────────
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default function Layout({ children }) {
+export default function Layout({ data: { menusJson: { header } }, children }) {
   return (
     <Wrapper>
       <Header>
-        <Link to="/">
-          <Heading>mrozilla</Heading>
-        </Link>
+        <Heading>
+          <Link to="/">mrozilla</Link>
+        </Heading>
+        <Nav>
+          <Nav.List>
+            {header.map(item => (
+              <Nav.List.Item>
+                <Link to={item.url}>{item.text}</Link>
+              </Nav.List.Item>
+            ))}
+          </Nav.List>
+        </Nav>
       </Header>
       {children()}
     </Wrapper>
   );
 }
+
+export const query = graphql`
+  query Menus {
+    menusJson {
+      header {
+        url
+        text
+      }
+    }
+  }
+`;
