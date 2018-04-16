95e085a858adc05d495e259aa2cc7b041b9cf6de
jan <jan@mrozilla.cz>
2018-04-16 23:53:34 +0200

    abstract header block component

---
+++ src/components/blocks/index.js
@@ -0,0 +1,1 @@
+export { default as HeaderBlock } from './HeaderBlock';
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './layout';
 export * from './typography';
 export * from './navigation';
+export * from './blocks';
