17557bf142eea6ec2851f378581edf7adb6b5ec2
jan <jan@mrozilla.cz>
2018-04-18 17:58:27 +0200

    add home hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,54 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 import React from 'react';
+import Helmet from 'react-helmet';
+import { Main, Section, Heading } from '../components';
 
 // ─────────────────────────────────────────────────────────────────────────────
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default function HomePage() {
+export default function HomePage({
+  data: { pagesJson: { meta, body: { hero } } },
+}) {
+  const availability = new Date();
+  availability.setMonth(availability.getMonth() + 1);
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Main>
+      <Helmet
+        title={`mrozilla: ${meta.title}`}
+        meta={[
+          { name: 'description', content: meta.description },
+          { name: 'keywords', content: meta.keywords },
+        ]}
+      />
+      <Section>
+        <Heading
+          fontSize="5rem"
+          fontWeight="400"
+          lineHeight="6rem"
+          margin="-1rem 0 10vh 0"
+        >
+          {hero.title}
+        </Heading>
+      </Section>
+    </Main>
   );
 }
+
+export const query = graphql`
+  query HomePage {
+    pagesJson(meta: { permalink: { eq: "/" } }) {
+      meta {
+        title
+        description
+        keywords
+      }
+      body {
+        hero {
+          title
+        }
+      }
+    }
+  }
+`;
