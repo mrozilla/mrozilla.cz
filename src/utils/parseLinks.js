48dad8924912a1ca14729bbaf254e73e265c4f55
jan <jan@mrozilla.cz>
2018-05-10 23:19:00 +0200

    add parse links helper

---
+++ src/utils/parseLinks.js
@@ -0,0 +1,28 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+import React from 'react';
+import { Link } from '../components';
+
+// ─────────────────────────────────────────────────────────────────────────────
+// component
+// ─────────────────────────────────────────────────────────────────────────────
+
+export default (str) => {
+  if (typeof str === 'string') {
+    const re = /\[(.*?\]\(.*?)\)/;
+    return str.split(re).map((item) => {
+      const [text, href] = item.split('](');
+      if (href) {
+        return (
+          <Link to={href} type="primary">
+            {text}
+          </Link>
+        );
+      }
+      return item;
+    });
+  }
+  return str;
+};
