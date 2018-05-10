d7ecc7d3ccc2082a1382c47662d5bce2b4f63aab
jan <jan@mrozilla.cz>
2018-05-10 23:20:43 +0200

    remove design mode toggle block

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -4,4 +4,4 @@ export { default as HeroBlock } from './HeroBlock';
 export { default as WorksBlock } from './WorksBlock';
 export { default as AvailabilityBlock } from './AvailabilityBlock';
 export { default as BlogBlock } from './BlogBlock';
-export { default as DesignModeToggleBlock } from './DesignModeToggleBlock';
+export { default as InactiveTabBlock } from './InactiveTabBlock';
