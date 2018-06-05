35e714032f496bea8286a1b582f0a35ef8a8f7a9
jan <jan@mrozilla.cz>
2018-06-05 11:32:14 +0200

    add ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: process.env.GATSBY_GA_TRACKING_ID,
+        head:       true,
+        anonymize:  true,
+        respectDNT: true,
+      },
+    },
     'gatsby-transformer-json',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
