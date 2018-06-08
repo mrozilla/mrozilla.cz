2b733723e673023346c6512c53128a840fc0424d
jan <jan@mrozilla.cz>
2018-06-08 07:47:49 +0200

    add radio component

--- src/components/input/Radio.js
+++ src/components/input/Radio.js
@@ -2,17 +2,52 @@
 // import
 // ─────────────────────────────────────────────────────────────────────────────
 
+import React from 'react';
 import styled from 'styled-components';
+import { Label } from '../../components';
 
 // ─────────────────────────────────────────────────────────────────────────────
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default styled.label`
-  text-transform: uppercase;
-  font-size: 1.25rem;
-  line-height: 2rem;
-  color: hsla(var(--hsl-text), 0.5);
-
-  padding: ${({ padding }) => padding};
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: flex-end;
+  margin: ${({ margin = '0 0 1rem 0' }) => margin};
 `;
+
+const StyledInput = styled.input`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+  outline: none;
+
+  box-shadow: inset 0 0 0 1px hsla(var(--hsl-text), 0.5);
+  transition: 250ms;
+
+  &:checked {
+    box-shadow: inset 0 0 0 6px hsl(var(--hsl-info));
+  }
+}
+
+input:checked {
+  border: 6px solid black;
+}
+`;
+
+export default function Radio({
+  name, label = 'input', margin, ...rest
+}) {
+  return (
+    <Wrapper margin={margin}>
+      <Label htmlFor={name}>{label}</Label>
+      <StyledInput id={name} type="radio" name={name} {...rest} />
+    </Wrapper>
+  );
+}
