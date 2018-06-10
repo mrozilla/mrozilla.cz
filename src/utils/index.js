572699973252d4e2036eb01290197325e6449626
jan <jan@mrozilla.cz>
2018-06-10 21:50:35 +0200

    add media query style helper

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as parseLinks } from './parseLinks';
 export { default as copyToClipboard } from './copyToClipboard';
 export { default as parseInput } from './parseInput';
+export { default as mediaQuerise } from './mediaQuerise';
