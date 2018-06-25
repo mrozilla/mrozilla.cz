13884c5e3df894da1f85ba8566d480d3fb654edc
jan <jan@mrozilla.cz>
2018-06-25 21:04:21 +0200

    update markdown renderer logic

--- src/utils/renderMarkdown.js
+++ src/utils/renderMarkdown.js
@@ -4,7 +4,18 @@
 
 import React from 'react';
 import RehypeReact from 'rehype-react';
+import { node } from 'prop-types';
 import { Link } from '../components/navigation';
+
+const BlogLink = ({ children, ...rest }) => (
+  <Link {...rest} type="primary">
+    {children}
+  </Link>
+);
+
+BlogLink.propTypes = {
+  children: node.isRequired,
+};
 
 // ─────────────────────────────────────────────────────────────────────────────
 // component
@@ -13,7 +24,7 @@ import { Link } from '../components/navigation';
 export default new RehypeReact({
   createElement: React.createElement,
   components:    {
-    a: Link,
+    a: BlogLink,
     // blockquote: BlockQuoteContainer, // TODO add tweetable quotes
   },
 }).Compiler;
