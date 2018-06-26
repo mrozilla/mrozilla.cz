96bd1ff4969842d041775e7d4cf77a4fc519b04b
jan <jan@mrozilla.cz>
2018-06-26 18:35:11 +0200

    update menu content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 import React from 'react';
-import Helmet from 'react-helmet';
 import {
   Main, Section, Subheading, Text,
 } from '../components';
