69279573bd133aaf93905802caf19984534c7155
jan <jan@mrozilla.cz>
2018-07-09 22:45:10 +0200

    add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,72 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+import React from 'react';
+import { Main, Section, Subheading } from '../components';
+import {
+  HeroContainer,
+  BlogPreviewsContainer,
+  SEOContainer,
+} from '../containers';
+
+// ─────────────────────────────────────────────────────────────────────────────
+// component
+// ─────────────────────────────────────────────────────────────────────────────
+
+export default function BlogPage({
+  data: {
+    pagesJson: {
+      meta: seo,
+      body: { hero },
+    },
+    allMarkdownRemark: { edges: posts },
+  },
+}) {
+  return (
+    <Main gridTemplate="'hero' 'blog'" gridGap="10vh 4rem">
+      <SEOContainer {...{ seo }} />
+      <HeroContainer hero={hero} />
+      <Section gridArea="blog" id="blog">
+        <Subheading>all blog articles</Subheading>
+        <BlogPreviewsContainer
+          posts={posts.map(({ node: { frontmatter: post, timeToRead } }) => ({
+            ...post,
+            timeToRead,
+          }))}
+        />
+      </Section>
+    </Main>
+  );
+}
+
+export const query = graphql`
+  query BlogPage {
+    pagesJson(meta: { permalink: { eq: "/blog" } }) {
+      meta {
+        title
+        description
+      }
+      body {
+        hero {
+          title
+        }
+      }
+    }
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/blog/" } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          timeToRead
+          frontmatter {
+            permalink
+            title
+            date(formatString: "MMMM D, YYYY")
+          }
+        }
+      }
+    }
+  }
+`;
