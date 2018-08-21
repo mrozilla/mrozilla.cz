7745288ab9cb7d1782ad0113b57273a64a48225f
jan <jan@mrozilla.cz>
2018-08-21 23:04:59 +0200

    update hero container title prop

--- src/containers/HeroContainer.js
+++ src/containers/HeroContainer.js
@@ -3,7 +3,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 import React from 'react';
-import { shape, string } from 'prop-types';
+import { string } from 'prop-types';
 
 import { Heading } from '../components';
 
@@ -11,22 +11,14 @@ import { Heading } from '../components';
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default function HeroContainer({ hero }) {
+export default function HeroContainer({ title }) {
   return (
-    <Heading
-      fontSize="5rem"
-      fontWeight="400"
-      lineHeight="6rem"
-      margin="-1rem 0 0 0"
-      gridArea="hero"
-    >
-      {hero.title}
+    <Heading fontSize="5rem" fontWeight="400" lineHeight="6rem" margin="-1rem 0 0 0" gridArea="hero">
+      {title}
     </Heading>
   );
 }
 
 HeroContainer.propTypes = {
-  hero: shape({
-    title: string.isRequired,
-  }).isRequired,
+  title: string.isRequired,
 };
