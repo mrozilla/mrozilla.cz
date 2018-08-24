40e6bef75393783d9c49b4b41eb4b9a3c750f40a
jan <jan@mrozilla.cz>
2018-08-25 00:12:25 +0200

    add parse input float logic

--- src/utils/parseInput.js
+++ src/utils/parseInput.js
@@ -6,7 +6,7 @@ export default function parseInput(target) {
   }
   if (target.type === 'number') {
     return {
-      [target.name]: parseInt(target.value, 10),
+      [target.name]: target.step ? parseFloat(target.value) : parseInt(target.value, 10),
     };
   }
   return {
