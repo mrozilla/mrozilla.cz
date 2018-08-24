ce2a57267f2698734eaf07248bad747c673d0e5e
jan <jan@mrozilla.cz>
2018-08-25 00:12:16 +0200

    add alert component

---
+++ src/components/typography/Alert.js
@@ -0,0 +1,28 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+import styled, { css } from 'styled-components';
+
+// ─────────────────────────────────────────────────────────────────────────────
+// component
+// ─────────────────────────────────────────────────────────────────────────────
+
+export default styled.div`
+  /* display: inline-block; */
+  line-height: 3rem;
+  padding: 1rem 2rem;
+  margin: 0 0 1rem 0;
+  box-shadow: 0 0 0 1px;
+  border-radius: 0.25rem;
+
+  ${({ type }) => {
+    if (type === 'danger') {
+      return css`
+        background-color: hsla(var(--hsl-danger), 0.05);
+        color: var(--color-danger);
+      `;
+    }
+    return null;
+  }};
+`;
