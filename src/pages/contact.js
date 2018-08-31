183f7c545057cc1a11f1af58c6e777dfd2e8fcfd
jan <jan@mrozilla.cz>
2018-08-31 19:56:54 +0200

    update contact inputs logic

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,11 +6,9 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import {
-  Main, Section, P, Link, Button,
+  Main, Section, P, Link, Button, Form, Input,
 } from '../components';
-import {
-  RootContainer, HeroContainer, SEOContainer, ContactContainer,
-} from '../containers';
+import { RootContainer, HeroContainer, SEOContainer } from '../containers';
 
 // ─────────────────────────────────────────────────────────────────────────────
 // query
@@ -25,6 +23,13 @@ export const query = graphql`
           title
         }
         form {
+          inputs {
+            name
+            label
+            type
+            rows
+            required
+          }
           success
           back
         }
@@ -48,7 +53,13 @@ export default function ContactPage({
 }) {
   return (
     <RootContainer>
-      <Main gridTemplate="'hero' 'form'" gridGap="10vh 4rem">
+      <Main
+        gridTemplate={{
+          xs: "'hero' 'form'",
+          lg: "'hero .' 'form .' / 1fr 1fr",
+        }}
+        gridGap="10vh 4rem"
+      >
         <SEOContainer meta={meta} />
         <HeroContainer title={hero.title} />
         {location.hash === '#success' ? (
@@ -59,7 +70,27 @@ export default function ContactPage({
             </Link>
           </Section>
         ) : (
-          <ContactContainer />
+          <Form
+            name="contact"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            method="POST"
+            action="/contact#success"
+            gridTemplate={{
+              xs: "'email' 'name' 'project' 'submit'",
+              lg: "'email name' 'project project' 'submit submit'",
+            }}
+            gridGap="0 1rem"
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="bot-field" />
+            {form.inputs.map(input => (
+              <Input {...input} key={input.name} />
+            ))}
+            <Button type="submit" margin="2rem 0 0 0" style={{ gridArea: 'submit' }}>
+              Send
+            </Button>
+          </Form>
         )}
       </Main>
     </RootContainer>
