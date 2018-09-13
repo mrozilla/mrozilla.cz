4e58387ab8eefe81732a193ca53f2b9258655a24
jan <jan@mrozilla.cz>
2018-09-13 16:30:07 +0200

    update input component

--- src/components/layout/Tooltip.js
+++ src/components/layout/Tooltip.js
@@ -3,19 +3,30 @@
 // ─────────────────────────────────────────────────────────────────────────────
 
 import styled from 'styled-components';
-import { mediaQuerise } from '../../utils';
 
 // ─────────────────────────────────────────────────────────────────────────────
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default styled.form(
-  {
-    gridArea: 'form',
-    display:  'grid',
-  },
-  ({ gridGap, gridTemplate }) => ({
-    ...mediaQuerise({ gridTemplate }),
-    ...mediaQuerise({ gridGap }),
-  }),
-);
+export default styled.div`
+  position: absolute;
+  background-color: var(--color-info);
+  border-radius: 1rem;
+  color: white;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  padding: 1rem;
+  top: calc(100% + 1rem);
+  transition: 100ms;
+
+  &::after {
+    content: '';
+    position: absolute;
+    border-style: solid;
+    border-width: 0.5rem;
+    bottom: 100%;
+    left: 1rem;
+
+    border-color: transparent transparent var(--color-info) transparent;
+  }
+`;
