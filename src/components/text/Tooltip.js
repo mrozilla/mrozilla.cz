51b12a8f4cb31f911f3b2253e93d963f1b8f3063
jan <jan@mrozilla.cz>
2018-10-26 17:38:59 +0200

    update text components

--- src/components/text/Tooltip.js
+++ src/components/text/Tooltip.js
@@ -4,20 +4,32 @@
 
 import styled from 'styled-components';
 
-import { Text } from '../primitives';
-
 // ─────────────────────────────────────────────────────────────────────────────
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export const Label = styled(Text)`
-  animation: ${({ animation }) => animation};
+export const Tooltip = styled.div`
+  --color: var(--color-info);
+
+  position: absolute;
+  background-color: var(--color);
+  border-radius: 1rem;
+  color: white;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  padding: 1rem;
+  top: calc(100% + 1rem);
+  transition: 100ms;
+  z-index: var(--z-index-modal);
+
+  &::after {
+    content: '';
+    position: absolute;
+    border-style: solid;
+    border-width: 0.5rem;
+    bottom: 100%;
+    left: 1rem;
+
+    border-color: transparent transparent var(--color) transparent;
+  }
 `;
-Label.defaultProps = {
-  as:            'label',
-  display:       'block',
-  color:         'hsla(var(--hsl-text), 0.75)',
-  fontSize:      '1.25rem',
-  lineHeight:    '3rem',
-  textTransform: 'uppercase',
-};
