b35c68515043c55d9ddf8f4d9a6e200af00fee94
jan <jan@mrozilla.cz>
2018-11-27 23:10:35 +0100

    remove redundant favicon links

--- src/containers/SEOContainer.js
+++ src/containers/SEOContainer.js
@@ -75,47 +75,6 @@ export default class SEOContainer extends PureComponent {
                 rel:  'canonical',
                 href: `${siteUrl}${this.props.meta.permalink}`,
               },
-
-              {
-                rel:   'icon',
-                sizes: '48x48',
-                href:  'icons/icon-48x48.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '72x72',
-                href:  'icons/icon-72x72.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '96x96',
-                href:  'icons/icon-96x96.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '144x144',
-                href:  'icons/icon-144x144.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '192x192',
-                href:  'icons/icon-192x192.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '256x256',
-                href:  'icons/icon-256x256.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '384x384',
-                href:  'icons/icon-384x384.png',
-              },
-              {
-                rel:   'icon',
-                sizes: '512x512',
-                href:  'icons/icon-512x512.png',
-              },
             ]}
           >
             <html lang="en" />
