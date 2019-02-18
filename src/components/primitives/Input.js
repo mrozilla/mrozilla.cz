2e11b57207e3960770371902a7e660b1c7798e4c
jan <jan@mrozilla.cz>
2019-02-18 22:48:58 +0100

    add temporal inputs styles

--- src/components/primitives/Input.js
+++ src/components/primitives/Input.js
@@ -98,7 +98,7 @@ export const Input = styled(Text)`
    */
 
   &:required:not(:focus):not(:placeholder-shown) {
-    &:invalid {
+    &:invalid:not([type*='date']):not([type='time']):not([type='month']) {
       color: var(--color-danger);
       box-shadow: 0 1px 0 0 var(--color-danger);
 
@@ -130,9 +130,30 @@ export const Input = styled(Text)`
    * remove search input additional styling in webkit
    */
 
-  &::-webkit-calendar-picker-indicator,
-  &::-webkit-search-cancel-button {
+  &[type='search']::-webkit-calendar-picker-indicator,
+  &[type='search']::-webkit-search-cancel-button {
     display: none;
+  }
+
+  /**
+   * add temporal fields placeholder, doesn't work in Firefox
+   */
+
+  &[type*='date'],
+  &[type='time'],
+  &[type='month'] {
+    &::before {
+      content: attr(placeholder) ':';
+      opacity: 0.25;
+      margin: 0 1rem 0 0;
+    }
+
+    &::-webkit-calendar-picker-indicator {
+      color: var(--color-info);
+    }
+    &::-webkit-datetime-edit-text {
+      opacity: 0.5;
+    }
   }
 `;
 
