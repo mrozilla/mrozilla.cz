840a5c0fdf64e60e47e88ec6024a4413d40c3721
jan <jan@mrozilla.cz>
2019-03-06 23:31:55 +0100

    update seo container to function

--- src/containers/SEOContainer.js
+++ src/containers/SEOContainer.js
@@ -2,7 +2,7 @@
 // import
 // ─────────────────────────────────────────────────────────────────────────────
 
-import React, { PureComponent } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql, StaticQuery } from 'gatsby';
 import { shape, string } from 'prop-types';
@@ -11,77 +11,75 @@ import { shape, string } from 'prop-types';
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export default class SEOContainer extends PureComponent {
-  static propTypes = {
-    meta: shape({
-      title:       string.isRequired,
-      description: string.isRequired,
-      permalink:   string.isRequired,
-      ogImage:     shape({
-        childImageSharp: shape({
-          resize: shape({
-            src: string.isRequired,
-          }),
-        }),
-      }),
-    }).isRequired,
-  };
-
-  render() {
-    return (
-      <StaticQuery
-        query={graphql`
-          {
-            site {
-              siteMetadata {
-                siteUrl
-                title
-              }
+export default function SEOContainer({ meta: { title, description, permalink, ogImage } }) {
+  return (
+    <StaticQuery
+      query={graphql`
+        {
+          site {
+            siteMetadata {
+              siteUrl
+              title
             }
           }
-        `}
-        render={({
-          site: {
-            siteMetadata: { siteUrl, title },
-          },
-        }) => (
-          <Helmet
-            title={this.props.meta.title}
-            titleTemplate={`%s | ${title}`}
-            meta={[
-              { name: 'description', content: this.props.meta.description },
+        }
+      `}
+      render={({
+        site: {
+          siteMetadata: { siteUrl, siteTitle },
+        },
+      }) => (
+        <Helmet
+          title={title}
+          titleTemplate={`%s | ${siteTitle}`}
+          meta={[
+            { name: 'description', content: description },
 
-              { name: 'twitter:card', content: 'summary_large_image' },
-              { name: 'twitter:site', content: '@mrozilla' },
-              { name: 'twitter:title', content: this.props.meta.title },
-              { name: 'twitter:description', content: this.props.meta.description },
-              { name: 'twitter:creator', content: '@mrozilla' },
-              {
-                name:    'twitter:image',
-                content: `${siteUrl}${this.props.meta.ogImage.childImageSharp.resize.src}`,
-              },
+            { name: 'twitter:card', content: 'summary_large_image' },
+            { name: 'twitter:site', content: '@mrozilla' },
+            { name: 'twitter:title', content: title },
+            { name: 'twitter:description', content: description },
+            { name: 'twitter:creator', content: '@mrozilla' },
+            {
+              name:    'twitter:image',
+              content: `${siteUrl}${ogImage.childImageSharp.resize.src}`,
+            },
 
-              { property: 'og:title', content: `${this.props.meta.title}` },
-              { property: 'og:type', content: 'website' },
-              { property: 'og:url', content: `${siteUrl}${this.props.meta.permalink}` },
-              {
-                property: 'og:image',
-                content:  `${siteUrl}${this.props.meta.ogImage.childImageSharp.resize.src}`,
-              },
-              { property: 'og:description', content: this.props.meta.description },
-              { property: 'fb:app_id', content: process.env.GATSBY_FB_APP_ID },
-            ]}
-            link={[
-              {
-                rel:  'canonical',
-                href: `${siteUrl}${this.props.meta.permalink}`,
-              },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-        )}
-      />
-    );
-  }
+            { property: 'og:title', content: `${title}` },
+            { property: 'og:type', content: 'website' },
+            { property: 'og:url', content: `${siteUrl}${permalink}` },
+            {
+              property: 'og:image',
+              content:  `${siteUrl}${ogImage.childImageSharp.resize.src}`,
+            },
+            { property: 'og:description', content: description },
+            { property: 'fb:app_id', content: process.env.GATSBY_FB_APP_ID },
+          ]}
+          link={[
+            {
+              rel:  'canonical',
+              href: `${siteUrl}${permalink}`,
+            },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+      )}
+    />
+  );
 }
+
+SEOContainer.propTypes = {
+  meta: shape({
+    title:       string.isRequired,
+    description: string.isRequired,
+    permalink:   string.isRequired,
+    ogImage:     shape({
+      childImageSharp: shape({
+        resize: shape({
+          src: string.isRequired,
+        }),
+      }),
+    }),
+  }).isRequired,
+};
