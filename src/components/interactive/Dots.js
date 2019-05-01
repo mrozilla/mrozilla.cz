3fa3ecd7001df7bd87dbb6ae209472cbf42bdc85
jan <jan@mrozilla.cz>
2019-05-01 20:56:17 +0200

    add dots component

---
+++ src/components/interactive/Dots.js
@@ -0,0 +1,28 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// import
+// ─────────────────────────────────────────────────────────────────────────────
+
+import React from 'react';
+
+import { View } from '../primitives/View';
+
+// ─────────────────────────────────────────────────────────────────────────────
+// component
+// ─────────────────────────────────────────────────────────────────────────────
+
+export default function Dots({ size, amount, orientation, gap, colors, ...rest }) {
+  return (
+    <View display="grid" gridGap={gap} {...rest}>
+      {Array.from({ length: amount }, (_, i) => i).map((dot, i) => (
+        <View key={dot} height={size} width={size} backgroundColor={colors[i]} borderRadius="50%" />
+      ))}
+    </View>
+  );
+}
+
+Dots.defaultProps = {
+  size:        '0.5rem',
+  amount:      3,
+  orientation: 'row',
+  gap:         '0.25rem',
+};
